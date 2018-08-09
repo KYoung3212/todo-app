6bbccff8611b558befbed02d1d19e49d065526d3
kyoung3212 <kevin.young3212@gmail.com>
2018-08-09 12:02:41 -0700

    Added todo list dummy data

---
+++ src/data/todo.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        titls: 'Go to store',
+        details: 'Get eggs, milk, bread, and Diet Coke',
+        _id: '001'
+    },{
+        title: 'Wash car',
+        details: 'Inside and out',
+        _id: '002'
+    },
+    {
+        title: 'React Vid',
+        details: 'watchandcode',
+        _id: '003'
+    },
+    {
+        title: 'Do Prototype',
+        details: 'Pull request',
+        _id: '004'
+    },
+    {
+        title: 'Watch tutorial',
+        details: 'Youtube',
+        _id: '005'
+    }
+];
