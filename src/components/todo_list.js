d5a57325d8bf61174e8dde56a02581421822b442
kyoung3212 <kevin.young3212@gmail.com>
2018-08-09 14:26:21 -0700

    lunch break

---
+++ src/components/todo_list.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import listData from '../data/todo';
+
+class TodoList extends Component{
+    constructor(props){{
+        super(props);
+    }}
+    render(){
+        console.log(listData);
+        const listElements = listData.map((item, index) => {
+            return <li className = 'collection-item' key = {item._id}>{item.title}</li>  
+        });
+        return (
+                <ul className = "collection">{listElements}</ul>
+        )
+    }
+}
+export default TodoList;
